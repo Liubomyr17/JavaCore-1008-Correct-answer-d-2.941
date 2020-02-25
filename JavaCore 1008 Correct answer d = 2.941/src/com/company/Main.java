abaa9918e069949bb51921e655e913dde666db48
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-25 10:42:05 +0200

    Add files via upload

---
+++ JavaCore 1008 Correct answer d = 2.941/src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+/*
+
+1008 Correct answer: d = 2.941
+Add one type conversion operation to get the answer: d = 2.941.
+Output Example:
+2.9411764705882355
+
+Requirements:
+1. The program should display the text on the screen.
+2. You cannot change the display command.
+3. The main () method must contain a variable of type int.
+4. The main () method must contain a variable b of type int.
+5. The main () method must contain a d variable of type double.
+6. The initial value of the variables during initialization cannot be changed. You can add only cast operators.
+7. The program should print the number 2.9411764705882355.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 17;
+        double d = (double) a / b;
+        System.out.println(d);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
